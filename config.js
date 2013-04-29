87f531061cbd4ac09717eae5a38f6224f0d4ff35
raytin <superRaytin@163.com>
2013-04-29 15:00:16 +0800

    密集测试&开发

--- config.js
+++ config.js
@@ -14,7 +14,7 @@
      uploadTempDir: './tmp', // 临时文件存放目录
      nopic: '/img/nopic.jpg', // 用户无头像图片
      limit: 15, // 每页数量
-     pageRange: 3, // 分页范围(指当前页码两边个数)
+     pageRange: 2, // 分页范围(指当前页码两边个数)
      key: 'r9g4w3a2', // 密钥
      cookieSecret: 'chihuohuo',
      sessionSecret: 'chihuohuo'
